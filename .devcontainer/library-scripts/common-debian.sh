ea43508a733492ec28fa1cf1323467547e30497b
Yi JIN <rickyking@users.noreply.github.com>
2020-12-15 12:09:27 -0500

    Update common-debian.sh

--- .devcontainer/library-scripts/common-debian.sh
+++ .devcontainer/library-scripts/common-debian.sh
@@ -22,6 +22,11 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+rm -f /etc/profile.d/00-restore-env.sh
+echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\${PATH}" > /etc/profile.d/00-restore-env.sh
+chmod +x /etc/profile.d/00-restore-env.sh
+
 # If in automatic mode, determine if a user already exists, if not use vscode
 if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
     USERNAME=""
@@ -92,6 +97,7 @@ if [ "${PACKAGES_ALREADY_INSTALLED}" != "true" ]; then
         dialog \
         libc6 \
         libgcc1 \
+        libkrb5-3 \
         libgssapi-krb5-2 \
         libicu[0-9][0-9] \
         liblttng-ust0 \
@@ -100,7 +106,8 @@ if [ "${PACKAGES_ALREADY_INSTALLED}" != "true" ]; then
         locales \
         sudo \
         ncdu \
-        man-db"
+        man-db \
+        strace"
 
     # Install libssl1.1 if available
     if [[ ! -z $(apt-cache --names-only search ^libssl1.1$) ]]; then
@@ -153,14 +160,14 @@ if id -u ${USERNAME} > /dev/null 2>&1; then
 else
     # Create user
     if [ "${USER_GID}" = "automatic" ]; then
-        groupadd $USERNAME
+        groupadd -f $USERNAME
     else
-        groupadd --gid $USER_GID $USERNAME
+        groupadd -f --gid $USER_GID $USERNAME
     fi
     if [ "${USER_UID}" = "automatic" ]; then 
-        useradd -s /bin/bash --gid $USERNAME -m $USERNAME
+        useradd -f -s /bin/bash --gid $USERNAME -m $USERNAME
     else
-        useradd -s /bin/bash --uid $USER_UID --gid $USERNAME -m $USERNAME
+        useradd -f -s /bin/bash --uid $USER_UID --gid $USERNAME -m $USERNAME
     fi
 fi
 
@@ -181,7 +188,6 @@ fi
 # .bashrc/.zshrc snippet
 RC_SNIPPET="$(cat << EOF
 export USER=\$(whoami)
-
 export PATH=\$PATH:\$HOME/.local/bin
 EOF
 )"
@@ -189,13 +195,10 @@ EOF
 # code shim, it fallbacks to code-insiders if code is not available
 cat << 'EOF' > /usr/local/bin/code
 #!/bin/sh
-
 get_in_path_except_current() {
   which -a "$1" | grep -v "$0" | head -1
 }
-
 code="$(get_in_path_except_current code)"
-
 if [ -n "$code" ]; then
   exec "$code" "$@"
 elif [ "$(command -v code-insiders)" ]; then
@@ -224,6 +227,13 @@ prompt() {
     fi
     local cwd="\$(pwd | sed "s|^\${HOME}|~|")"
     PS1="\${green}\${USERNAME} \${arrow_color}➜\${reset_color} \${bold_blue}\${cwd}\${reset_color} \$(scm_prompt_info)\${white}$ \${reset_color}"
+    
+    # Prepend Python virtual env version to prompt
+    if [[ -n \$VIRTUAL_ENV ]]; then
+        if [ -z "\${VIRTUAL_ENV_DISABLE_PROMPT:-}" ]; then
+            PS1="(\`basename \"\$VIRTUAL_ENV\"\`) \${PS1:-}"
+        fi
+    fi
 }
 SCM_THEME_PROMPT_PREFIX="\${reset_color}\${cyan}(\${bold_red}"
 SCM_THEME_PROMPT_SUFFIX="\${reset_color} "
